b9674b839ff56d2071fbff592324289b6db850e7
Mayank Pujari <90626561+SkillShotGod@users.noreply.github.com>
2022-07-06 19:21:40 +0530

    Forgot a save

--- src/components/navsidebar/NavSidebar.jsx
+++ src/components/navsidebar/NavSidebar.jsx
@@ -2,7 +2,12 @@ import {Link} from 'react-router-dom'
 import ChangePassword from '../../pages/account/ChangePassword'
 import ProfileInformation from '../../pages/account/ProfileInformation'
 import { useState } from 'react'
+
+
+// This is the component for the sidebar to be used for navigation within the Account route. It was a possibility to simply make different pages for said routes and just use router for it but since we used router once for app.js, might as well just use something different
 const NavSidebar = ( {setCurrentElement, currentElement} ) => {
+
+  //function to switch between components. checking if we click on page we already are in, then we do not need to do anything at all
   const changeElement =(e)=>{
     if (e.target.id!==currentElement){
       setCurrentElement(e.target.id)
